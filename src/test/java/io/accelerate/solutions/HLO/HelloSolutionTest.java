1248381bb93b8fdb517b8e4b14f3b246d243ae80
sbx_user1051 <sbx_user1051@169.254.84.43>
2025-09-27 21:54:24 +0000

    Sat Sep 27 21:54:24 UTC 2025

---
+++ src/test/java/io/accelerate/solutions/HLO/HelloSolutionTest.java
@@ -0,0 +1,39 @@
+package io.accelerate.solutions.HLO;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+public class HelloSolutionTest {
+    private HelloSolution underTest;
+
+    @BeforeEach
+    public void setUp() {
+        underTest = new HelloSolution();
+    }
+
+    @Test
+    public void testHello() {
+        assertThat(underTest.hello("Name1"), equalTo("Hello Name1!"));
+    }
+
+    @ParameterizedTest
+    @MethodSource
+    public void testHelloStrings(String name, String expectedMessage) {
+        assertThat(underTest.hello(name), equalTo(expectedMessage));
+    }
+
+    private static Stream<Arguments> testHelloStrings() {
+        return Stream.of(
+                Arguments.of("Name A", "Hello Name A!"),
+                Arguments.of("FirstName lastName", "Hello FirstName lastName!")
+        );
+    }
+}
