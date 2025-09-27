bb9338cab93831788d35d4cc0580e56f679006aa
sbx_user1051 <sbx_user1051@169.254.2.93>
2025-09-27 21:15:46 +0000

    Sat Sep 27 21:15:46 UTC 2025

--- src/test/java/io/accelerate/solutions/SUM/SumSolutionTest.java
+++ src/test/java/io/accelerate/solutions/SUM/SumSolutionTest.java
@@ -2,7 +2,12 @@ package io.accelerate.solutions.SUM;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
+
+import java.util.stream.Stream;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
@@ -18,5 +23,19 @@ public class SumSolutionTest {
     @Test
     public void compute_sum() {
         assertThat(sum.compute(1, 1), equalTo(2));
+    }
+
+    @ParameterizedTest
+    @MethodSource
+    public void compute_sums(int x, int y, int expectedSum) {
+        assertThat(sum.compute(x, y), equalTo(expectedSum));
+    }
+
+    private static Stream<Arguments> compute_sums() {
+        return Stream.of(
+                Arguments.of(1, 2, 3),
+                Arguments.of(10, 20, 30),
+                Arguments.of(5, 10, 15)
+        );
     }
 }
